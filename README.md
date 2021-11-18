4e27db81e78b05e136e2544f3a0777408ff18476
ayush-shrivastava003 <77131808+ayush-shrivastava003@users.noreply.github.com>
2021-11-17 23:18:08 -0500

    strings and floats

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # language
 A simple programming language interpreted in Python. Inspired by https://github.com/tsj845/Custom-Language-Interpreter
+https://ruslanspivak.com/lsbasi-part1/
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import sys
 
 # declare token types
-EOF, INT, SPACE, ASSIGN, PLUS, MINUS, NEWLINE = "EOF", "INT", " ", "=", "+", "-", "\n"
-
+EOF, INT, SPACE, ASSIGN, PLUS, MINUS, NEWLINE, STRING, FLOAT = "EOF", "INT", "SPACE", "EQUAL", "PLUS", "MINUS", "NEWLINE", "STRING", "FLOAT"
 errorCodes = {
   0: "Syntax Error",
-  1: "Internal Error"
+  1: "Unkown Variable Error",
+  2: "Internal Error"
 }
 
 class Token():
@@ -15,6 +15,9 @@ class Token():
     """
     self.type = type
     self.value = value
+
+  def toString(self):
+    return f"Token({self.type}, {self.value})"
   
 class Interpreter():
   def __init__(self, content):
@@ -32,16 +35,16 @@ class Interpreter():
     self.content = content
     self.index = -1
     self.running = True
-    self.currentToken = None
+    self.currentToken = Token("START", "START") # arbitrary Token, just need it to be an instance of the class
     self.char = self.content[self.index]
     self.vars = {}
 
   def raiseError(self, code, reason):
     """
-    Outputs an error and terminates the program by setting `self.running` to false.
+    Outputs an error and terminates the program.
     """
     print(f"\u001b[31m{errorCodes[code]}:\u001b[0m {reason}") # the gibberish is an ANSI escape code to make the text red and cool
-    self.running = False
+    exit()
 
   def increment(self):
       """
@@ -52,20 +55,60 @@ class Interpreter():
       if self.index < len(self.content):
         self.char = self.content[self.index]
       else:
-        self.running = False
         self.char = None
 
-  def getInt(self):
+  def getWord(self):
     """
-    Scans the content for an integer, moving the pointer until it hits a space.
+    Scans the content for a word, moving the pointer until it hits a space.
+
+    Words can represent variables or function calls (functions and function calls are not implemented at the moment).
+
+    This function is also used as a helper function for `getInt` and `getFloat`.
     """
     final = ""
 
-    while self.char != " " and self.char != None: # checks for None in case the next char is EOF
+    while self.char != " " and self.char != None and self.char != "\n":
+      final += self.char
+      self.increment() # TODO: MAKE THIS CODE BLOCK INTO ITS OWN METHOD
+    return final
+
+  def getNum(self):
+    """
+    Scans the content for a number, moving the pointer until it hits a space.
+    Then it checks if the number is an integer or a floating point number.
+    """
+    final = self.getWord()
+    split = final.split('.')
+
+    if len(split) > 2:
+      self.raiseError(0, "Received multiple decimals in an expected floating point number")
+    elif len(split) == 1:
+      return int(final), INT
+
+    for num in split:
+      if num == '':
+        self.raiseError(0, "Expected value after decimal point")
+
+      elif not num.isdigit():
+        self.raiseError(0, "Unexpected character in float")
+
+    return final, FLOAT
+
+  def getString(self):
+    """
+    Scans the content for an string, moving the pointer until it hits an endquote.
+    Strings are only identified by double quotes (").
+    """
+    final = ""
+
+    while self.char != "\"":
+      if self.char == None:
+        self.raiseError(0, "Unterminated string before EOF")
+
       final += self.char
       self.increment()
 
-    return int(final)
+    return final
 
   def nextToken(self):
     """
@@ -81,13 +124,15 @@ class Interpreter():
       self.increment()
 
       if self.char == None:
+        self.running = False
         break
 
       elif self.char == " ":
         self.increment()
 
       if self.char.isdigit():
-        return Token(INT, self.getInt())
+        num, type = self.getNum()
+        return Token(type, num)
 
       elif self.char == "=":
         return Token(ASSIGN, "=")
@@ -101,27 +146,31 @@ class Interpreter():
       elif self.char == "+":
         return Token(PLUS, "+")
 
+      elif self.char == "\"":
+        self.increment() # move pointer to start of string rather than the quote
+        return Token(STRING, self.getString())
 
-      self.raiseError(0, f"Unidentified type '{self.char}'")
+
+      self.raiseError(0, f"Unexpected character '{self.char}'")
   
     return Token(EOF, None)
 
-  def verify(self, type):
-    """
-    Verify that the token at the current position is of the expected type.
-    """
-    if self.currentToken.type == type:
-      self.currentToken = self.nextToken() # since the type matches, we can move the pointer to the next self.character
-    else:
-      self.raiseError(1, f"Expected type {type} at position {self.index}, got {self.currentToken.type}")
+  # def verify(self, type):
+  #   """
+  #   Verify that the token at the current position is of the expected type.
+  #   """
+  #   if self.currentToken.type == type:
+  #     self.currentToken = self.nextToken() # since the type matches, we can move the pointer to the next self.character
+  #   else:
+  #     self.raiseError(1, f"Expected type {type} at position {self.index}, got {self.currentToken.type}")
 
   def run(self):
     """
     Continually calls `nextToken` until it has reached EOF, or an until exception has been raised.
     """
-    while self.running:
+    while self.running and self.currentToken.type != EOF:
       self.currentToken = self.nextToken()
-      print(self.currentToken, self.currentToken.value)
+      print(self.currentToken.toString())
   
 
 with open(sys.argv[1]) as file:
